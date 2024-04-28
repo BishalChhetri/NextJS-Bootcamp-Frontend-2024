e25804916be721332c33294967a0fc47854698f6
BishalChhhetri <kcbisal1@gmail.com>
2024-04-28 10:48:21 +0545

    fix: bugs

--- app/error.tsx
+++ app/error.tsx
@@ -11,9 +11,6 @@ interface ErrorStateProps {
 const ErrorState: React.FC<ErrorStateProps> = ({ error }) => {
   useEffect(() => {
     console.error(error);
-    if (error && error.message === "Connection closed.") {
-      window.location.reload();
-    }
   }, [error]);
   return (
     <div className="h-[60vh] flex flex-col gap-2 justify-center items-center">
