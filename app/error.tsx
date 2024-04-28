c9dbb6350cb0fae169923847a74ff227cb2a7c5d
BishalChhhetri <kcbisal1@gmail.com>
2024-04-28 09:26:46 +0545

    fix:bugs

--- app/error.tsx
+++ app/error.tsx
@@ -11,15 +11,16 @@ interface ErrorStateProps {
 const ErrorState: React.FC<ErrorStateProps> = ({ error }) => {
   useEffect(() => {
     console.error(error);
-    console.log({ error });
-    console.log({ errorMessage: error?.message });
+    if (error && error.message === "Connection closed.") {
+      window.location.reload();
+    }
   }, [error]);
   return (
     <div className="h-[60vh] flex flex-col gap-2 justify-center items-center">
       <EmptyState
         title={"Uh oh!"}
         subtitle={`${
-          error && error.message === "Error: Connection closed."
+          error && error.message === "Connection closed."
             ? "Connection closed due to prolonged inactivity. Please manually hard refresh."
             : "Something went wrong!"
         }`}
